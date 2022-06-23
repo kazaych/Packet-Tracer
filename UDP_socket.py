8bdb984e22b96474dd4b23e570211144ee3f3c92
Kazay_asus <Kazay@mail.ru>
2022-06-23 09:58:30 +0300

    migrate to class

---
+++ UDP_socket.py
@@ -0,0 +1,20 @@
+import socket
+
+
+class UdpSocket:
+    def __init__(self, prt, dle_s='1002', dle_e='1003'):
+        self.sock = None
+        self.dle_s = dle_s
+        self.dle_e = dle_e
+        self.prt = prt
+
+    def sockopen(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('', self.prt))  # open socket
+        data = self.sock.recv(1500)
+        return data
+
+
+prt = UdpSocket(4016)
+raw = prt.sockopen()
+print(raw)
--- main.py
+++ main.py
@@ -1,42 +1,37 @@
 import socket
 import keyboard
+import UDP_socket
 
 
-def uncut_buf(raw, cat):  # take data from socket and check that only one asterix packet in eth frame
-    if raw[0:2] == cat:
-        p_len = int(hex(int(raw[2:6], 16)), 16) * 2  # calc len of packet
-        pck_cut = raw[0:p_len]
-        if (len(raw) - len(pck_cut)) > 0:
-            return raw, 1
+    def uncut_buf(self, raw, cat):
+        if raw[0:2] == cat:
+            p_len = int(hex(int(raw[2:6], 16)), 16) * 2  # calc len of packet
+            pck_cut = raw[0:p_len]
+            if (len(raw) - len(pck_cut)) > 0:
+                  return raw, 1
+            else:
+                 return pck_cut, 0
         else:
-            return pck_cut, 0
-    else:
-        return raw, 1
+             return raw, 1
+
+    def buffer_cut(self, raw, cat):  #
+        pck_list = []
+        while len(raw) > 0:
+            pck_begin = raw.find(cat)
+            pck_len = int(hex(int(raw[pck_begin + 2: pck_begin + 6], 16)), 16) * 2  # calc len of packet
+             frame = raw[pck_begin:pck_len]
+             pck_list.append(frame)
+             raw = raw[pck_len:]  # cut frame from raw
+        return pck_list
 
 
-def buffer_cut(raw, cat):  # take data from buffer only if It's not only one asterix frame in one eth frame
-    pck_list = []
-    while len(raw) > 0:
-        pck_begin = raw.find(cat)
-        pck_len = int(hex(int(raw[pck_begin + 2: pck_begin + 6], 16)), 16) * 2  # calc len of packet
-        frame = raw[pck_begin:pck_len]
-        pck_list.append(frame)
-        raw = raw[pck_len:]  # cut frame from raw
-    return pck_list
-
-
-print('Input Asterix cat. num')
-input_cat = str(hex(int(input())))
-cat_num = input_cat[2:]
-print('Input port UDP')
-udp_port = int(input())
-
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('', udp_port))  # open socket
+input_cat = str(hex(int(input())))[2:]
+port = int(input())
 raw_buf = ''  # buffering data if not one cat  packet in eth frame
+
+data = UdpSocket(4001)
+data.sock =
 while True:
-    data = sock.recv(1515)
     raw_str_buf = str(data.hex())
     pck, flg = uncut_buf(raw_str_buf, cat_num)
     if flg == 1:  # if in uncut_buf not one cat packet ++ to raw buf
@@ -52,6 +47,6 @@ while True:
         else:
             print('mapped', pck)
             print(150 * '-')
-    if keyboard.is_pressed('q'): # press q to stop socket
+    if keyboard.is_pressed('q'):  # press q to stop socket
         print('q has pressed end of program')
         break
